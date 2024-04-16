16595e237362eff81146f208dc86f6570ef0baf8
mabubakar365 <m.abubakar365@yahoo.com>
2024-04-17 02:24:49 +0500

    Added code for Assignment 4

--- Typescript/Assignments/Assignment4/Exercise01/index.ts
+++
@@ -1,6 +0,0 @@
-// 1. Hello Variable
-
-// Declare a variable named 'greeting' of type string and assign the value "Hello, World!" to it.
-let greeting: string = "Hello, World!"; 
-// Use the console.log function to print the value of the 'greeting' variable to the console.
-console.log(greeting);
